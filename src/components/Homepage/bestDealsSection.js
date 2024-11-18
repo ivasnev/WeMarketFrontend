c23bb8d962798d8859e85ce334d258a09b0ad6b3
Илья Васнев <ivasnev2002@gmail.com>
2024-11-18 04:42:33 +0300

    Сделана главная

--- src/components/Homepage/bestDealsSection.js
+++ src/components/Homepage/bestDealsSection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import bestDealsSectionStyles from './bestDealsSection.module.scss';
+import productData from "../../mockedData/products";
+import ProductCard from "../ProductCard";
 
 function renderBestDealsSection(props) {
   return (
@@ -17,46 +19,58 @@ function renderBestDealsSection(props) {
             {/* TODO */}
             <div className={bestDealsSectionStyles.btn_text}>Browse All Product</div>
             <img
-              className={bestDealsSectionStyles.btn_icon}
-              src={'/assets/6f85c022597fa57a644395d5915b740f.svg'}
-              alt="alt text"
+                className={bestDealsSectionStyles.btn_icon}
+                src={'/assets/6f85c022597fa57a644395d5915b740f.svg'}
+                alt="alt text"
             />
           </button>
         </div>
-
         <div className={bestDealsSectionStyles.productGrid}>
-          {/* Grid layout for product cards */}
-
-          <div className={bestDealsSectionStyles.contentBox2}>
-            <div className={bestDealsSectionStyles.unnamed}>
-              <img
-                className={bestDealsSectionStyles.image28}
-                src={'/assets/c7ef5bd9dc392f52c81c13d0d3ab5853.png'}
-                alt="alt text"
+          {productData.slice(0,10).map((product) => (
+              <ProductCard
+                  key={product.id}
+                  className={bestDealsSectionStyles.contentBox2}
+                  contentBox={product.contentBox}
+                  image28={product.image24} // Подставляем данные из мока
+                  paragraph={product.text7}
+                  text3={product.text9}
               />
-              <p className={bestDealsSectionStyles.paragraph}>4K UHD LED Smart TV with Chromecast Built-in</p>
-
-              <div className={bestDealsSectionStyles.text3}>
-                <div className={bestDealsSectionStyles.text31}>$1,50</div>
-              </div>
-            </div>
-          </div>
-
-          <div className={bestDealsSectionStyles.contentBox2}>
-            <div className={bestDealsSectionStyles.unnamed}>
-              <img
-                className={bestDealsSectionStyles.image28}
-                src={'/assets/c7ef5bd9dc392f52c81c13d0d3ab5853.png'}
-                alt="alt text"
-              />
-              <p className={bestDealsSectionStyles.paragraph}>4K UHD LED Smart TV with Chromecast Built-in</p>
-
-              <div className={bestDealsSectionStyles.text3}>
-                <div className={bestDealsSectionStyles.text31}>$1,50</div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
+
+        {/*<div className={bestDealsSectionStyles.productGrid}>*/}
+        {/*  /!* Grid layout for product cards *!/*/}
+
+        {/*  <div className={bestDealsSectionStyles.contentBox2}>*/}
+        {/*    <div className={bestDealsSectionStyles.unnamed}>*/}
+        {/*      <img*/}
+        {/*          className={bestDealsSectionStyles.image28}*/}
+        {/*          src={'/assets/c7ef5bd9dc392f52c81c13d0d3ab5853.png'}*/}
+        {/*          alt="alt text"*/}
+        {/*      />*/}
+        {/*      <p className={bestDealsSectionStyles.paragraph}>4K UHD LED Smart TV with Chromecast Built-in</p>*/}
+
+        {/*      <div className={bestDealsSectionStyles.text3}>*/}
+        {/*        <div className={bestDealsSectionStyles.text31}>$1,50</div>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+
+        {/*  <div className={bestDealsSectionStyles.contentBox2}>*/}
+        {/*    <div className={bestDealsSectionStyles.unnamed}>*/}
+        {/*      <img*/}
+        {/*          className={bestDealsSectionStyles.image28}*/}
+        {/*          src={'/assets/c7ef5bd9dc392f52c81c13d0d3ab5853.png'}*/}
+        {/*          alt="alt text"*/}
+        {/*      />*/}
+        {/*      <p className={bestDealsSectionStyles.paragraph}>4K UHD LED Smart TV with Chromecast Built-in</p>*/}
+
+        {/*      <div className={bestDealsSectionStyles.text3}>*/}
+        {/*        <div className={bestDealsSectionStyles.text31}>$1,50</div>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </section>
   );
